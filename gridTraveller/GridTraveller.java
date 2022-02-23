e3a61961337eaac6c6b958ecc0e893ef109d596e
Suracho <suracho@outlook.com>
2022-02-23 17:11:45 +0530

    added memoization to gridTraveller

--- gridTraveller/GridTraveller.java
+++ gridTraveller/GridTraveller.java
@@ -1,20 +1,24 @@
 package com.suracho.gridTraveller;
 //refer to freecodecamps dynamic programming video for question
 public class GridTraveller {
+    static int[][] dp = new int[18][18];
+
     public static int gridTraveller(int row,int column){
         if(row == 0 || column == 0){
             return 0;
         }
-
-        if(row==1 || column==1){
+        if(dp[row-1][column-1]!=0){
+            return dp[row-1][column-1];
+        }else if(row==1 && column==1){
             return 1;
+        } else {
+            dp[row-1][column-1]= gridTraveller(row-1,column)+gridTraveller(row,column-1);
+            return dp[row-1][column-1];
         }
-
-        return gridTraveller(row-1,column) + gridTraveller(row,column-1);
-
     }
 
     public static void main(String[] args) {
-        System.out.println(gridTraveller(3,3));
+        System.out.println(gridTraveller(2,3));
+
     }
 }
