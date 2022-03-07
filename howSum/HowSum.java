22e5e4b29cb32fdd07424701eafcda1b22f4d243
Suracho <suracho@outlook.com>
2022-03-07 21:04:16 +0530

    added memoization to howSum

--- howSum/HowSum.java
+++ howSum/HowSum.java
@@ -2,14 +2,19 @@ package com.suracho.howSum;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class HowSum {
 
     static ArrayList<Integer> collectNum = new ArrayList<>();
 
-    private static ArrayList<Integer> makeHowSum(int targetSum, ArrayList<Integer> numbers) {
+    private static ArrayList<Integer> makeHowSum(int targetSum, ArrayList<Integer> numbers, HashMap<Integer,ArrayList<Integer>> memo) {
         if(targetSum==0){
             return collectNum;
+        }
+
+        if (memo.containsKey(targetSum)){
+            return memo.get(targetSum);
         }
 
         if (targetSum<0){
@@ -18,17 +23,21 @@ public class HowSum {
 
         for (int i = 0; i < numbers.size(); i++) {
             int remainder = targetSum - numbers.get(i);
-            if(makeHowSum(remainder,numbers)!=null){
+            if(makeHowSum(remainder,numbers,memo)!=null){
                 collectNum.add(numbers.get(i));
-                return collectNum;
+                memo.put(targetSum, collectNum);
+
+                return memo.get(targetSum);
             }
         }
 
+        memo.put(targetSum,null);
         return null;
     }
 
     public static void main(String[] args) {
-        ArrayList<Integer> arr = new ArrayList<Integer>(Arrays.asList(5,3,4,7));
-        System.out.println(makeHowSum(7,arr));
+        ArrayList<Integer> arr = new ArrayList<Integer>(Arrays.asList(7,14));
+        HashMap<Integer,ArrayList<Integer>> memo = new HashMap<>();
+        System.out.println(makeHowSum(300,arr,memo));
     }
 }
